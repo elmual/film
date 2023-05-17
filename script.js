074f24dbd87f08cdc2c82ee03bd545eae0fb2c99
erlams <el@gmail.com>
2023-05-17 22:05:04 +0400

    Loop elave edildi

--- script.js
+++ script.js
@@ -8,9 +8,30 @@ const personalMovieDB = {
     privat: false
 }
 
-let a = prompt('Axirinci baxdiginiz film');
-let b = prompt('Nech bal ile qiymetlendirirsiniz,');
 
-personalMovieDB.movies[a] = b;
+for (let i = 0; i < 2; i++) {
+
+    let a = prompt('Axirinci baxdiginiz film');
+    let b = prompt('Nech bal ile qiymetlendirirsiniz,');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB[a] = b;
+        console.log('Hazir');
+    } else {
+        console.log('Sehv var');
+        i--;
+    }
+    
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Siz heveskasiniz');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Siz klassik izleyicisiniz');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Siz kinomensiniz');
+} else {
+    console.log('Sehv var');
+}
 
 console.log(personalMovieDB);
